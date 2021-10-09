8848060520ffe9b5666cba56074f96cb7ed7b8a6
bogdanpoz <bogdanpozdev@gmail.com>
2021-10-09 03:13:05 +0300

    just upload

---
+++ resources/views/posts/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+      <title>Laravel 8 CRUD Application - laravelcode.com</title>
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">  </head>
+  <body>
+    
+    <div class="container">
+        @yield('content')
+    </div>
+    
+  </body>
+</html>
